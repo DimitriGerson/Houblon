5a99e66d650a2b976495ed6793d81d991b89c6fe
DimitriGerson <51483045+DimitriGerson@users.noreply.github.com>
2025-11-19 22:25:57 +0100

    Update test_wifi_utils.py
    retour au code de base.

--- tests/test_wifi_utils.py
+++ tests/test_wifi_utils.py
@@ -17,9 +17,8 @@ cfg_sta = {
 }
 
 def test_disable_all_wifi():
-    with pytest.raises(Exception) as exc:
-        wifi_utils.disable_all_wifi()
-    assert False, f"Une exception a été levée : {exc}"
+    wifi_utils.disable_all_wifi()
+    assert True   # pas d'erreur = succès
 
 def test_start_ap_returns_wlan():
     ap = wifi_utils.start_ap(cfg_ap)
